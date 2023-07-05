00016b69b4f220e0774139666794647eb42ed2e8
langochai <langochai11121998@gmail.com>
2023-07-05 09:18:35 +0700

    fix the broken clock

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-const time =
-    <div>
-        <h1>It is {new Date().toLocaleTimeString()}.</h1>
-    </div>
-root.render(time)
+
+const tick = () =>{
+    const time =
+        <div>
+            <h1>It is {new Date().toLocaleTimeString()}.</h1>
+        </div>
+    root.render(time)
+}
+setInterval(tick,1000)
